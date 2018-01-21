60f9189c0bff729847d234f74d4ac8fdf4d4fb31
a1301720 <kathrin@kronfuss.at>
2018-01-22 00:47:42 +0100

    POST registration added

--- dist/register.js
+++ dist/register.js
@@ -2,6 +2,8 @@
 // user registration
 Object.defineProperty(exports, "__esModule", { value: true });
 const swagger = require("swagger-node-express");
+// Needed for password check
+const crypto = require("crypto");
 class Register {
     constructor(pool, users) {
         this.pool = pool;
@@ -59,21 +61,49 @@ class Register {
     doPostRegister(req) {
         return new Promise((resolve, reject) => {
             // be sure not to OVERWRITE existing users (Only INSTERT, not UPDATE)
-            let sql = "";
+            let requiredFields = ["pk_username", "pwhash", "email"];
+            for (let i = 0; i < requiredFields.length; i++) {
+                if (!req.body.hasOwnProperty(requiredFields[i])) {
+                    reject(`Missing field: ${requiredFields[i]}`);
+                    return;
+                }
+            }
+            let sql = "INSERT INTO users(";
             let params = [];
+            let allFields = ["pk_username", "pwhash", "email", "name", "surname", "companyname", "billingaddress", "deliveryaddress", "vat", "nameoncc", "creditcardnr", "validyear", "validmonth", "ccv"];
+            let i = 0;
+            for (; i < allFields.length; i++) {
+                if (req.body.hasOwnProperty(allFields[i])) {
+                    if (i != 0) {
+                        sql += `, `;
+                    }
+                    sql += ` ${allFields[i]}`;
+                    if (allFields[i] == "pwhash") {
+                        params.push(crypto.createHash('sha256').update(req.body[allFields[i]]).digest('base64'));
+                    }
+                    else {
+                        params.push(req.body[allFields[i]]);
+                    }
+                }
+            }
+            sql += ", isadmin";
+            params.push(false);
+            sql += `) VALUES( `;
+            for (let j = 0; j < params.length; j++) {
+                if (j != 0) {
+                    sql += `, `;
+                }
+                sql += `$${j + 1}`;
+            }
+            sql += `)`;
             this.pool
                 .query(sql, params)
-                .then(res => {
-                if (res.rows.length == 1) {
-                    resolve(res.rows);
-                }
-                else {
-                    reject("Failed");
-                }
-            })
                 .catch(error => {
-                console.error(sql + " with params " + JSON.stringify(params) + ": " + error.toString());
-                reject(error.toString());
+                console.error("User already existing");
+                reject("User already existing");
+            })
+                .then(res => {
+                resolve(res.rows);
             });
         });
     }
