f1f051a2639cbb14287478de7b560a832cabf6bf
a1269039 <a1269039@unet.univie.ac.at>
2018-01-22 22:52:24 +0100

    finished shoppingcart.ts

--- src/shoppingcart.ts
+++ src/shoppingcart.ts
@@ -20,8 +20,8 @@ import { Request } from "express-serve-static-core";
 
 export class ShoppingCart {
 
-  // private defaultLimit : number = 100;
-  // private defaultOffset = 0;
+   private defaultLimit : number = 100;
+   private defaultOffset = 0;
 
   constructor (private pool, private users) {
   }
@@ -29,10 +29,10 @@ export class ShoppingCart {
   public mount () {
     swagger
     .addGet (this.getCartItems)
-    //.addGet (this.getOrdersByID)
-    //.addPut(this.putOrdersByID)
-    //.addPost (this.postOrders)
-    //.addDelete(this.deleteOrders)
+    .addGet (this.getShoppingCart)
+    .addPut(this.putCartItemByID)
+    .addPost (this.postCartItem)
+    .addDelete(this.deleteShoppingCartItem)
     // .addPost (this.postUserWithQueryParameter)
     // .addPost (this.postUsers)
     // .addDelete (this.deleteUserById)
@@ -49,7 +49,7 @@ export class ShoppingCart {
   public getCartItems = {
     'spec': {
       description : "Operations about shopping cart",
-      path : "/cart/{username}",
+      path : "/cart/{id}",
       method: "GET",
       summary : "Get cart for specific user",
       notes : "Returns cart items",
@@ -57,7 +57,7 @@ export class ShoppingCart {
       nickname : "getCartItems",
       produces : ["application/json"],
        parameters : [
-          swagger.params.path("username", "username of the user", "string")
+          swagger.params.path("id", "id of the shoppingCartItem", "long")
         ],
       responseMessages : [  
 	  { "code": 400, "message": 'invalid name' },
@@ -78,18 +78,158 @@ export class ShoppingCart {
   };
 
 
+  public deleteShoppingCartItem = {
+    'spec': {
+      description : "Operations about ShoppingCartItem",
+      path : "/cart/{id}",
+      method: "DELETE",
+      summary : "delete the selected shopping cart item",
+      notes : "Returns number of deleted items",
+      type : "count",
+      nickname : "deleteCartItem",
+      produces : ["application/json"],
+      parameters : [
+        swagger.params.path("id", "id of the shopping cart item", "long")
+      ],
+      responseMessages : [{
+        "code": 500,
+        "message": 'internal server error'
+      }]
+    },
+    'action': (req,res) => {
+      this.doDeleteShoppingcartitems (req)
+      .then (result => {
+          res.send(JSON.stringify(result))
+      })
+      .catch (error => res.status(500).send ({
+         "code": 500,
+         "message": error
+      }))
+    }
+  };
+  
+  
+   public getShoppingCart = {
+    'spec': {
+      description : "Operations about Shopping Cart Items",
+      path : "/cart/",
+      method: "GET",
+      summary : "Get one shopping cart collection for one user",
+      notes : "Returns a shopping cart collection for one user",
+      type : "cart",
+      nickname : "getShoppingCartItems",
+      produces : ["application/json"],
+      parameters : [
+        swagger.params.query ("limit", "Limit number of results", "number", false, null, this.defaultLimit),
+        swagger.params.query ("offset", "Use together with 'limit' for paging", "number", false, null, 0)
+        ],
+      responseMessages : [
+        { "code": 400, "message": 'invalid id' },
+        // { "code": 404, "message": 'id not found' },
+        { "code": 500, "message": 'internal server error'}
+      ]
+    },
+    'action': (req,res) => {
+      if (!req.params.id) {
+        throw swagger.errors.invalid('id');
+      }
+      let id = parseInt(req.params.id);
+      if (isNaN(id)) {
+        throw swagger.errors.invalid('id');
+      }
+      this.doGetCart (req, req.auth, req.query.limit, 
+          req.query.offset)
+      .then (result => res.send(JSON.stringify(result)))
+      .catch (error => res.status(500).send ({
+         "code": 500,
+         "message": error
+      }))
+    }
+  };
+
+  
+    public postCartItem = {
+    'spec': {
+      description : "Operations about Shopping Cart Items",
+      path : "/cart",
+      method: "POST",
+      summary : "Create an Shopping Cart Item",
+      notes : "Returns shoppingCartItemID",
+      type : "cart",
+      nickname : "PostCart",
+      produces : ["application/json"],
+      parameters : [
+    //    swagger.params.path ("username", "UserName of User", "string"), 
+        swagger.params.body("body", 'Order as JSON string', "string")
+      ],
+      responseMessages : [
+        { "code": 400, "message": 'invalid parameter' },
+        // { "code": 404, "message": 'id not found' },
+        { "code": 500, "message": 'internal server error'}
+      ]
+    },
+    'action': (req,res) => {
+      this.doPostCartItem (req, req.auth, req.params.id)
+      .then (result => res.send(JSON.stringify(result)))
+      .catch (error => res.status(500).send ({
+         "code": 500,
+         "message": error
+      }))
+    }
+  };
+
+
+  public putCartItemByID = {
+    'spec': {
+      description : "Operation to put Shopping Cart Items",
+      path : "/cart/{id}",
+      method: "PUT",
+      summary : "Put one specific Shopping Cart Item",
+      notes : "Returns the cartID",
+      type : "cart",
+      nickname : "putCartItemByID",
+      produces : ["application/json"],
+      parameters : [
+          swagger.params.path("id", "ID of the cart item", "long")
+        ],
+      responseMessages : [
+        { "code": 400, "message": 'invalid id' },
+        // { "code": 404, "message": 'id not found' },
+        { "code": 500, "message": 'internal server error'}
+      ]
+    },
+    'action': (req,res) => {
+      if (!req.params.id) {
+        throw swagger.errors.invalid('id');
+      }
+      let id = parseInt(req.params.id);
+      if (isNaN(id)) {
+        throw swagger.errors.invalid('id');
+      }
+      this.doPutShoppingcartitemsByID (req, req.auth, req.params.id)
+      .then (result => res.send(JSON.stringify(result)))
+      .catch (error => res.status(500).send ({
+         "code": 500,
+         "message": error
+      }))
+    }
+  };
+
+
   ///////////////////////////////////////////////
   ///
   ///  DB access methods
 
-     public doGetCartItems (req: Request) : Promise<Types.CartItems[]> {
+  public doGetCartItems (req: Request) : Promise<Types.CartItems[]> {
     return new Promise ((resolve, reject) => {
       if (! req.hasOwnProperty ('auth')) {
         return reject ("Not logged in");
       }
-      let sql = "SELECT * FROM shoppingcartitems where fk_pk_username = $1";
+      let sql = "SELECT * FROM shoppingcartitems where fk_pk_username = $1"
+        + " AND pk_cartid = $2";
 	  console.log("this is the query" + sql);
-      let params: [string | number] = [req.params.username];
+      let params: [string | number] = [req.auth.username, 
+        req.params.id];
       this.pool
       .query (sql, params)
       .then (res => {
@@ -106,6 +246,162 @@ export class ShoppingCart {
     });
   }
 
+  public doGetCart (req: Request, auth: Types.Auth, limit: number, offset: number) : Promise<Types.Order[]> {
+    return new Promise ((resolve, reject) => {
+      if (! req.hasOwnProperty ('auth')) {
+        return reject ("Not logged in");
+      }
+      let sql = "SELECT * FROM shoppingcartitems WHERE "
+       + "fk_pk_username=" + auth.user;
+      let params: [string | number] = [limit || this.defaultLimit, offset || 0];
+      sql += "  LIMIT $1 OFFSET $2";
+      this.pool
+      .query (sql, params)
+      .then (res => {
+        resolve (res.rows);
+      })
+      .catch (error => {
+        console.error(sql + " with params "+JSON.stringify (params)+": " + error.toString());
+        reject (error.toString());
+      });
+    });
+  }
 
+  public doPostCartItem (req: Request, auth: Types.Auth, 
+    id: number) : Promise<Types.Id> {
+   return new Promise ((resolve, reject) => {
+      if (! req.hasOwnProperty ('auth')) {
+        return reject ("Not logged in");
+      }
+      
+      let requiredFields = ["pk_cartid", "amount", 
+      "price", "fk_pk_username", "fk_pk_productid"];
+
+      for(let i = 0; i < requiredFields.length; i++){
+        if(! req.body.hasOwnProperty(requiredFields[i])){
+          reject(`Missing field: ${requiredFields[i]}` );
+          return;
+        }
+      }
+
+	  // create query for insert into orders table
+      let sql2 = "INSERT INTO shoppingcartitems(";
+      let params2 = [];
+
+      let allFieldsSql2 = ["pk_cartid", "amount", 
+        "price", "fk_pk_username", "fk_pk_productid"];
+      let i = 0;
+
+      for(; i < allFieldsSql2.length; i++){
+        if(req.body.hasOwnProperty(allFieldsSql2[i])){
+          if(i != 0){
+            sql2 += `, `;
+          }
+          sql2 += ` ${allFieldsSql2[i]}`;
+            params2.push(req.body[allFieldsSql2[i]]);
+        }
+      }
+	  
+	 // let orderdate = req.body.orderdate; 
+
+      sql2 += `) VALUES (`;
+      for(let j = 0; j < i; j++){
+        if(j != 0){
+          sql2 += `, `;
+        }
+        sql2 += `$${j+1}`;
+
+      }
+	  	  
+	  sql2 += `) `;
+	  
+	  // check 
+	    console.log(sql2);
+      console.log(JSON.stringify(params2));
+	  
+	  //insert into db
+	  this.pool
+		.query (sql2, params2)
+        .catch ( error => {
+          console.error(sql2 + " with params "+JSON.stringify (params2) + ": " + error.toString());
+          reject (error.toString());
+        });
+	  
+	  // create query for insert into orderitems table
+      });
+
+    }
+
+    
+    public doDeleteShoppingcartitems (req: Request) : 
+    Promise<Types.Count> {
+      return new Promise ((resolve, reject) => {
+        if (! req.hasOwnProperty ('auth')) {
+          return reject ("Not logged in");
+        }
+  
+        let sql = "DELETE FROM shoppingcartitems where " + 
+        "pk_cartid = $1 RETURNING *";
+        let params: [number] = [req.params.id];
+        this.pool
+        .query (sql, params)
+        .then (res => {
+          if(res.rows.length == 1){
+            resolve (res.rows);
+          } else {
+              reject ("No such shoppingcartitems");
+            }
+        })
+        .catch (error => {
+          console.error(sql + " with params "+JSON.stringify (params)+": " + error.toString());
+          reject (error.toString());
+        });
+      });
+    }
+
+    public doPutShoppingcartitemsByID(req:Request, 
+      auth:Types.Auth, id:number){
+        return new Promise ((resolve, reject) => {
+          if (! req.hasOwnProperty ('auth')) {
+            return reject ("Not logged in");
+          }
+
+          let requiredFields = ["pk_cartid", "amount", 
+          "price", "fk_pk_username", "fk_pk_productid"];
+
+        // create query for insert into orders table
+          let sql2 = "UPDATE INTO shoppingcartitems SET ";
+
+          let allFieldsSql2 = ["pk_cartid", "amount", 
+            "price", "fk_pk_username", "fk_pk_productid"];
+          let i = 0;
+
+          for(; i < allFieldsSql2.length; i++){
+            if(req.body.hasOwnProperty(allFieldsSql2[i])){
+              if(i != 0){
+                sql2 += `, `;
+              }
+              sql2 += `${allFieldsSql2[i]}` + " = '"
+              + `${req.body[allFieldsSql2[i]]}` + "'";
+            }
+          }
+        
+      // let orderdate = req.body.orderdate; 
+            
+        sql2 += " WHERE pk_cartid = " + 
+         `${id}`;
+        
+        // check 
+        console.log(sql2);
+        
+        //insert into db
+        this.pool
+        .query (sql2)
+            .catch ( error => {
+              console.error(sql2 + ": " + error.toString());
+              reject (error.toString());
+            });
+        });
+      }
 
   }
