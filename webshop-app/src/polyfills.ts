2b168c088fd0c60e979f1cf121b071299290d467
Katharina Kutschera <kathi.kutschera@gmx.at>
2018-01-22 17:22:25 +0100

    @gui: browser support

--- webshop-app/src/polyfills.ts
+++ webshop-app/src/polyfills.ts
@@ -43,6 +43,7 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
@@ -64,3 +65,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'web-animations-js';
