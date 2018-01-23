76becf862334273ab1cc07bc2c1ca885a1a777d1
a1269039 <a1269039@unet.univie.ac.at>
2018-01-23 19:30:54 +0100

    small error pruning

--- webshop-app/src/app/shoppingcart/shoppingcart.component.ts
+++ webshop-app/src/app/shoppingcart/shoppingcart.component.ts
@@ -25,7 +25,6 @@ export class ShoppingcartComponent implements OnInit {
     this.webshopService.
       getCart().then((data) => {
         console.log("data: " + JSON.stringify(data, null, 2));
-        this.cart = data;
         
         
       }).catch(err => {
