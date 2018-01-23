196074bf00c70ddd3a7a98cf7ec592c3def8b249
a1301720 <kathrin@kronfuss.at>
2018-01-23 10:55:18 +0100

    adapt name search

--- src/products.ts
+++ src/products.ts
@@ -384,11 +384,11 @@ public getProductGroupById = {
             }
             if(req.query.name){
                 if(counter >=1){
-                    sql += " AND products.name ~ $"+(counter+3);
+                    sql += " AND UPPER(products.name) ~ UPPER($"+(counter+3)+")";
                 }else{
-                    sql += " products.name ~ $"+(counter+3);
+                    sql += " UPPER(products.name) ~ UPPER($"+(counter+3)+")";
                 }
-                params.push(req.query.name+'*');
+                params.push(req.query.name);
                 counter += 1;
             }
             if(req.query.keyword){
